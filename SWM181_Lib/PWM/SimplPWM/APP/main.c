3d57e9cb3a6fb90dc45ad5135f3989b201dfbd76
Synwit-tech <78465039+Synwit-tech@users.noreply.github.com>
2021-02-05 09:41:13 +0800

    Initial commit

---
+++ SWM181_Lib/PWM/SimplPWM/APP/main.c
@@ -0,0 +1,35 @@
+#include "SWM181.h"
+
+
+int main(void)
+{
+	PWM_InitStructure  PWM_initStruct;
+	
+	SystemInit();
+	
+	PWM_initStruct.clk_div = PWM_CLKDIV_4;		//F_PWM = 24M/4 = 6M
+	
+	PWM_initStruct.mode = PWM_MODE_INDEP;		//A路和B路独立输出					
+	PWM_initStruct.cycleA = 10000;				//6M/10000 = 600Hz			
+	PWM_initStruct.hdutyA =  2500;				//2500/10000 = 25%
+	PWM_initStruct.initLevelA = 1;
+	PWM_initStruct.cycleB = 10000;
+	PWM_initStruct.hdutyB =  5000;				//5000/10000 = 50%
+	PWM_initStruct.initLevelB = 1;
+	PWM_initStruct.HEndAIEn = 0;
+	PWM_initStruct.NCycleAIEn = 0;
+	PWM_initStruct.HEndBIEn = 0;
+	PWM_initStruct.NCycleBIEn = 0;
+	
+	PWM_Init(PWM1, &PWM_initStruct);
+	
+	PORT_Init(PORTE, PIN0, FUNMUX_PWM1A_OUT, 0);
+	PORT_Init(PORTE, PIN1, FUNMUX_PWM1B_OUT, 0);
+	
+	PWM_Start(PWM1, 1, 1);
+	
+	while(1==1)
+	{
+	
+	}
+}
---
+++ SWM181_Lib/PWM/SimplPWM/out/startup_swm181.d
@@ -0,0 +1,1 @@
+.\out\startup_swm181.o: ..\..\SWM181_StdPeriph_Driver\CSL\CMSIS\DeviceSupport\startup\arm\startup_SWM181.s
